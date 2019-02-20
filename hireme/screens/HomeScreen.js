9c22bc82e24a769925679d6cd7d2c05d04e50f88
mohsinansari3011 <mohsinansari3011@gmail.com>
2019-02-20 20:30:59 +0500

    Setting up alert

--- hireme/screens/HomeScreen.js
+++ hireme/screens/HomeScreen.js
@@ -32,7 +32,7 @@ export default class HomeScreen extends React.Component {
     if (type === 'success') {
       // Get the user's name using Facebook's Graph API
       const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`);
-      Alert.alert('Logged in!', `Hi ${(await response.json()).name}!`);
+      alert('Logged in!', `Hi ${(await response.json()).name}!`);
     } else {
       // type === 'cancel'
     }
