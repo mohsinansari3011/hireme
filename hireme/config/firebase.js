47f152a6af747edc42b0ce23b2c4f78dd9e5641b
mohsinansari3011 <mohsinansari3011@gmail.com>
2019-02-20 21:59:16 +0500

    Settings for firebase...

---
+++ hireme/config/firebase.js
@@ -0,0 +1,34 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyAZNORQAH7vJisy0Jk-RMWbsL-oZ7wRzmY",
+    authDomain: "hireme-3011.firebaseapp.com",
+    databaseURL: "https://hireme-3011.firebaseio.com",
+    projectId: "hireme-3011",
+    storageBucket: "hireme-3011.appspot.com",
+    messagingSenderId: "954926979787"
+};
+
+firebase.initializeApp(config);
+
+
+const firedb = firebase.database();
+
+
+const firebaselooper = (snapshot) => {
+    const data = [];
+    snapshot.forEach((childSnapshot) => {
+        data.push({
+            ...childSnapshot.val(),
+            id: childSnapshot.key
+        })
+    });
+
+    return data;
+}
+
+
+
+export {
+    firebase, firedb
+}
--- hireme/screens/HomeScreen.js
+++ hireme/screens/HomeScreen.js
@@ -9,6 +9,8 @@ import {
   View, Button,
 } from 'react-native';
 
+import { firebase, firedb } from '../config/firebase';
+const firebase_users = firedb.ref('users');
 
 //import { LoginManager } from "react-native-fbsdk";
 
@@ -42,6 +44,9 @@ export default class HomeScreen extends React.Component {
       
       //console.log(response.json());
       alert(`Logged in! Hi ${profile.name} , ${profile.email}!`);
+
+      this.SubmitDataToFirebase(profile);
+
     } else {
       // type === 'cancel'
     }
@@ -50,6 +55,63 @@ export default class HomeScreen extends React.Component {
   }
 }
 
+
+  SubmitDataToFirebase = (dataToSubmit) =>{
+
+    dataToSubmit['password'] = 'password';
+    firebase.auth().createUserWithEmailAndPassword(dataToSubmit.email, dataToSubmit.password)
+    .then(() => {
+      
+
+
+      firebase_users.orderByChild("id")
+        .limitToLast(1).once('value')
+        .then((snapshot) => {
+
+          let userId = null;
+          snapshot.forEach(childsnapshot => {
+            userId = childsnapshot.val().id;
+          })
+
+          //console.log(this.state.formdata.category.value);
+          dataToSubmit['id'] = userId + 1;
+          dataToSubmit['date'] = firebase.database.ServerValue.TIMESTAMP;
+          dataToSubmit['isdelete'] = false;
+          dataToSubmit['isblock'] = false;
+          dataToSubmit['location'] = { cord: { lat: '0.004044', long: '0.0004' } };
+          dataToSubmit['phone'] = '0303-6660032';
+
+
+          firebase_users.push(dataToSubmit)
+            .then(() => {
+              // this.setState({
+              //   loading: false,
+              //   registorCompleted: 'User Inserted Successfully',
+
+              // })
+
+            }).catch(error => {
+              // this.setState({
+              //   loading: false,
+              //   registorError: error.message
+              // })
+            })
+
+        })
+
+
+
+
+    }).catch(error => {
+      // this.setState({
+      //   loading: false,
+      //   registorError: error.message
+      // })
+    })
+
+
+
+}
 
   renderButton = () =>{
       return (
