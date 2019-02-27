a8674a72597e535b4f8839772c4bb92e47b0eb80
mohsinansari3011 <mohsinansari3011@gmail.com>
2019-02-27 21:41:59 +0500

    Update LoginScreen.js

--- hireme/screens/LoginScreen.js
+++ hireme/screens/LoginScreen.js
@@ -15,7 +15,133 @@ export default class LoginScreen extends React.Component {
 
     render() {
         return (
-            <Text> loginScreen </Text>
+            <View style={styles.container}>
+                <Text style={styles.label}>Welcome to the HireMe Application!!</Text>
+                <TouchableOpacity onPress={() => this._handleLogin()}>
+
+                    <Image
+                        source={
+                            require('../assets/images/loginfacebook.png')
+                        }
+                        style={styles.FbImage}
+                    />
+                    {/* <Button
+              onPress={() => this._handleLogin()}
+              title="Login With Facebook"
+              color="#841584"
+              accessibilityLabel="Learn more about this purple button"
+            /> */}
+                </TouchableOpacity>
+            </View>
         );
     }
 }
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+
+
+        backgroundColor: '#F5FCFF',
+    }, label: {
+        fontSize: 16,
+        fontWeight: 'normal',
+        marginBottom: 48,
+    },
+    developmentModeText: {
+        marginBottom: 20,
+        color: 'rgba(0,0,0,0.4)',
+        fontSize: 14,
+        lineHeight: 19,
+        textAlign: 'center',
+    }, label: {
+        fontSize: 16,
+        fontWeight: 'normal',
+        marginBottom: 48,
+    },
+    contentContainer: {
+        paddingTop: 30,
+    },
+    welcomeContainer: {
+        alignItems: 'center',
+        marginTop: 10,
+        marginBottom: 20,
+    },
+    welcomeImage: {
+        width: 100,
+        height: 80,
+        resizeMode: 'contain',
+        marginTop: 3,
+        marginLeft: -10,
+    }, FbImage: {
+        flex: 1,
+        width: 100,
+        height: 80,
+        resizeMode: 'contain',
+        marginTop: 3,
+        marginLeft: -10,
+    },
+    getStartedContainer: {
+        alignItems: 'center',
+        marginHorizontal: 50,
+    },
+    homeScreenFilename: {
+        marginVertical: 7,
+    },
+    codeHighlightText: {
+        color: 'rgba(96,100,109, 0.8)',
+    },
+    codeHighlightContainer: {
+        backgroundColor: 'rgba(0,0,0,0.05)',
+        borderRadius: 3,
+        paddingHorizontal: 4,
+    },
+    getStartedText: {
+        fontSize: 17,
+        color: 'rgba(96,100,109, 1)',
+        lineHeight: 24,
+        textAlign: 'center',
+    },
+    tabBarInfoContainer: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        ...Platform.select({
+            ios: {
+                shadowColor: 'black',
+                shadowOffset: { height: -3 },
+                shadowOpacity: 0.1,
+                shadowRadius: 3,
+            },
+            android: {
+                elevation: 20,
+            },
+        }),
+        alignItems: 'center',
+        backgroundColor: '#fbfbfb',
+        paddingVertical: 20,
+    },
+    tabBarInfoText: {
+        fontSize: 17,
+        color: 'rgba(96,100,109, 1)',
+        textAlign: 'center',
+    },
+    navigationFilename: {
+        marginTop: 5,
+    },
+    helpContainer: {
+        marginTop: 15,
+        alignItems: 'center',
+    },
+    helpLink: {
+        paddingVertical: 15,
+    },
+    helpLinkText: {
+        fontSize: 14,
+        color: '#2e78b7',
+    },
+});
