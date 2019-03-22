3047801be5ca12b8fc70f98fa667d0d0d94ef8c3
mohsinansari3011 <mohsinansari3011@gmail.com>
2019-03-22 22:03:21 +0500

    Update SettingsScreen.js

--- hireme/screens/SettingsScreen.js
+++ hireme/screens/SettingsScreen.js
@@ -315,7 +315,7 @@ export default class SettingsScreen extends React.Component {
 
 
   renderButton = () => {
-    //const { user } = this.state;
+    const { user } = this.state;
     //alert(this.state.user); 
     return (
       <View style={styles.container}>
@@ -356,7 +356,7 @@ export default class SettingsScreen extends React.Component {
 
   render() {
     const { user } = this.state;
-    console.log('render user ', user);
+    //console.log('render user ', user);
     return (
       <View style={styles.container}>
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
