9a23fd03070b72c67cb4591ecbdbca4743b21729
mohsinansari3011 <mohsinansari3011@gmail.com>
2019-04-09 20:28:11 +0500

    Update LoginScreen.js

--- hireme/screens/LoginScreen.js
+++ hireme/screens/LoginScreen.js
@@ -181,7 +181,7 @@ export default class LoginScreen extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        //flex: 1,
         flexDirection: 'column',
 
 
